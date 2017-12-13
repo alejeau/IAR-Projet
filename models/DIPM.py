de625b6a4518c465ca4827616450d73040172eca
alejeau <aure.lejeau@gmail.com>
2017-12-13 15:37:35 +0100

    minor update

--- models/DIPM.py
+++ models/DIPM.py
@@ -44,78 +44,78 @@ class DIPM:
         return self.theta_gpi
 
     # StratiumD1
-    def u_d1(self, y_c: float) -> float:
+    def u_i_d1(self, y_c: float) -> float:
         return self.wcs1 * y_c
 
-    def y_d1(self, a_d1: float) -> float:
-        return self.m * (a_d1 - self.theta_d1) * Tools.heaviside_step_function(a_d1 - self.theta_d1)
+    def y_i_d1(self, a_i_d1: float) -> float:
+        return self.m * (a_i_d1 - self.theta_d1) * Tools.heaviside_step_function(a_i_d1 - self.theta_d1)
 
     # StratiumD2
-    def u_d2(self, y_c: float) -> float:
+    def u_i_d2(self, y_c: float) -> float:
         return self.wcs2 * y_c
 
-    def y_d2(self, a_d2: float) -> float:
-        return self.m * (a_d2 - self.theta_d2) * Tools.heaviside_step_function(a_d2 - self.theta_d2)
+    def y_i_d2(self, a_i_d2: float) -> float:
+        return self.m * (a_i_d2 - self.theta_d2) * Tools.heaviside_step_function(a_i_d2 - self.theta_d2)
 
     # GPe
-    def u_gpe(self, y_d2: float) -> float:
+    def u_i_gpe(self, y_d2: float) -> float:
         return (-self.wsd2_gpe) * y_d2
 
-    def y_gpe(self, a_gpe: float) -> float:
-        return self.m * (a_gpe - self.theta_gpe) * Tools.heaviside_step_function(a_gpe - self.theta_gpe)
+    def y_i_gpe(self, a_i_gpe: float) -> float:
+        return self.m * (a_i_gpe - self.theta_gpe) * Tools.heaviside_step_function(a_i_gpe - self.theta_gpe)
 
     # STN
-    def u_stn(self, y_gpe: float) -> float:
+    def u_i_stn(self, y_gpe: float) -> float:
         return (-self.wgpe_stn) * y_gpe
 
-    def y_stn(self, a_stn: float) -> float:
-        return self.m * (a_stn - self.theta_stn) * Tools.heaviside_step_function(a_stn - self.theta_stn)
+    def y_i_stn(self, a_i_stn: float) -> float:
+        return self.m * (a_i_stn - self.theta_stn) * Tools.heaviside_step_function(a_i_stn - self.theta_stn)
 
     # GPi
-    def u_gpi(self, y_sd1: float, stn_list: [float]) -> float:
+    def u_i_gpi(self, y_sd1: float, stn_list: [float]) -> float:
         return (-self.wsd1_gpi) * y_sd1 + self.stn_gpi * sum(stn_list)
 
-    def y_gpi(self, a_gpi: float) -> float:
-        return self.m * (a_gpi - self.theta_gpi) * Tools.heaviside_step_function(a_gpi - self.theta_gpi)
+    def y_i_gpi(self, a_i_gpi: float) -> float:
+        return self.m * (a_i_gpi - self.theta_gpi) * Tools.heaviside_step_function(a_i_gpi - self.theta_gpi)
 
     def delta_a(self, a: float, u: float) -> float:
         return a - self.k * (a - u) * self.dt
 
     def compute_stn(self, salience) -> float:
-        # we have the salience, we now need a_xxx
+        # we have the salience, we now need a_i_xxx
         # we also probably need to store the past values
 
         # d1
-        u_d1 = self.u_d1(salience)
+        u_d1 = self.u_i_d1(salience)
         self.a_d1 = self.delta_a(self.a_d1, u_d1)
         # print('delta_d1: ' + str(self.a_d1))
-        self.y_d1 = self.y_d1(self.a_d1)
+        self.y_d1 = self.y_i_d1(self.a_d1)
 
         # d2
-        u_d2 = self.u_d2(salience)
+        u_d2 = self.u_i_d2(salience)
         self.a_d2 = self.delta_a(self.a_d2, u_d2)
         # print('delta_d2: ' + str(self.a_d2))
-        y_d2 = self.y_d2(self.a_d2)
+        y_d2 = self.y_i_d2(self.a_d2)
 
         # gpe
-        u_gpe = self.u_gpe(y_d2)
+        u_gpe = self.u_i_gpe(y_d2)
         self.a_gpe = self.delta_a(self.a_gpe, u_gpe)
         # print('delta_gpe: ' + str(self.a_gpe))
-        y_gpe = self.y_gpe(self.a_gpe)
+        y_gpe = self.y_i_gpe(self.a_gpe)
 
         # stn
-        u_stn = self.u_stn(y_gpe)
+        u_stn = self.u_i_stn(y_gpe)
         self.a_stn = self.delta_a(self.a_stn, u_stn)
         # print('delta_stn: ' + str(self.a_stn))
-        y_stn = self.y_stn(self.a_stn)
+        y_stn = self.y_i_stn(self.a_stn)
 
         return y_stn
 
     def compute_gpi(self, stn_list: [float]) -> float:
-        u_gpi = self.u_gpi(self.y_d1, stn_list)
+        u_gpi = self.u_i_gpi(self.y_d1, stn_list)
         self.a_gpi = self.delta_a(self.a_gpi, u_gpi)
         # print('delta_gpi: ' + str(self.a_gpi))
-        return self.y_gpi(self.a_gpi)
+        return self.y_i_gpi(self.a_gpi)
 
     def load_conf(self, filename: str):
         conf = confLoader.load(filename)
--- models/SCPM.py
+++ models/SCPM.py
@@ -43,63 +43,63 @@ class SCPM:
         self.y_d1 = 0.0
 
     # StratiumD1
-    def u_d1(self, y_c: float):
+    def u_i_d1(self, y_c: float):
         return self.wcs1 * y_c
 
-    def y_d1(self, a_d1: float):
+    def y_i_d1(self, a_d1: float):
         return self.m * (a_d1 - self.theta_d1) * Tools.heaviside_step_function(a_d1 - self.theta_d1)
 
     # StratiumD2
-    def u_d2(self, y_c: float):
+    def u_i_d2(self, y_c: float):
         return self.wcs2 * y_c
 
-    def y_d2(self, a_d2: float):
+    def y_i_d2(self, a_d2: float):
         return self.m * (a_d2 - self.theta_d2) * Tools.heaviside_step_function(a_d2 - self.theta_d2)
 
     # GPe
-    def u_gpe(self, y_d2: float, y_stn: list):
+    def u_i_gpe(self, y_d2: float, y_stn: list):
         return (-self.wsd2_gpe) * y_d2 + self.wstn_gpe * sum(y_stn)
 
-    def y_gpe(self, a_gpe: float):
+    def y_i_gpe(self, a_gpe: float):
         return self.m * (a_gpe - self.theta_gpe) * Tools.heaviside_step_function(a_gpe - self.theta_gpe)
 
     # STN
-    def u_stn(self, y_c: float, y_gpe: float):
+    def u_i_stn(self, y_c: float, y_gpe: float):
         return (-self.wc_stn) * y_c - self.wgpe_stn * y_gpe
 
-    def y_stn(self, a_stn: float):
+    def y_i_stn(self, a_stn: float):
         return self.m * (a_stn - self.theta_stn) * Tools.heaviside_step_function(a_stn - self.theta_stn)
 
     # GPi
-    def u_gpi(self, y_sd1: float, y_stn: list, y_gpe: float):
+    def u_i_gpi(self, y_sd1: float, y_stn: list, y_gpe: float):
         return (-self.wsd1_gpi) * y_sd1 + self.wstn_gpe * sum(y_stn) - self.wgpe_gpi * y_gpe
 
-    def y_gpi(self, a_gpi: float):
+    def y_i_gpi(self, a_gpi: float):
         return self.m * (a_gpi - self.theta_gpi) * Tools.heaviside_step_function(a_gpi - self.theta_gpi)
 
     def delta_a(self, a: float, u: float) -> float:
         return a - self.k * (a - u) * self.dt
 
     def compute_stn(self, salience) -> float:
-        # we have the salience, we now need a_xxx
+        # we have the salience, we now need a_i_xxx
         # we also probably need to store the past values
 
         # d1
-        u_d1 = self.u_d1(salience)
+        u_d1 = self.u_i_d1(salience)
         self.a_d1 = self.delta_a(self.a_d1, u_d1)
         # print('delta_d1: ' + str(self.a_d1))
-        self.y_d1 = self.y_d1(self.a_d1)
+        self.y_d1 = self.y_i_d1(self.a_d1)
 
         # d2
-        u_d2 = self.u_d2(salience)
+        u_d2 = self.u_i_d2(salience)
         self.a_d2 = self.delta_a(self.a_d2, u_d2)
         # print('delta_d2: ' + str(self.a_d2))
-        y_d2 = self.y_d2(self.a_d2)
+        y_d2 = self.y_i_d2(self.a_d2)
 
         # # stn
-        # u_stn = self.u_stn(y_gpe)
+        # u_stn = self.u_i_stn(y_gpe)
         # self.a_stn = self.delta_a(self.a_stn, u_stn)
         # # print('delta_stn: ' + str(self.a_stn))
-        # y_stn = self.y_stn(self.a_stn)
+        # y_stn = self.y_i_stn(self.a_stn)
 
         return y_stn
