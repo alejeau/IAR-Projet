8fdcb4f6bdfaeefde6e5c0dcddcb9fea277edcdd
Aurélien LEJEAU <aure.lejeau@gmail.com>
2017-12-15 16:35:02 +0100

    wip on pyplot

--- tools/display.py
+++ tools/display.py
@@ -8,13 +8,19 @@ def load_file():
 
 def display_data(gpi_outputs: {int: {int: float}}):
     print(gpi_outputs)
-    abscisses = range(5000, 5)
     ordonnees = [[], [],[]]
-    for channel in sorted(gpi_outputs.keys()):
+    channels = sorted(gpi_outputs.keys())
+    for channel in channels:
+        sample_size = len(gpi_outputs[channel])
+        # scale = sample_size / 1000.0
+        abscisses = range(5000, 5)
         for i in abscisses:
             ordonnees[channel].append(gpi_outputs[channel][i])
 
-
-        plt.subplot(131)
         plt.plot(abscisses, ordonnees[channel])
+
+        # rows, column, plot number
+        id = 100 + len(channels) * 10 + channel+1
+        print(id)
+        plt.subplot(id)
         plt.show()
