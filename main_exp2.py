8ab3e110c8c6c725c5673af9f468793161e92a48
Aurélien LEJEAU <aure.lejeau@gmail.com>
2017-12-22 18:10:55 +0100

    Added first computation of raw results for exp2

--- main_exp2.py
+++ main_exp2.py
@@ -6,6 +6,7 @@ import Simulator.SCPMSimulator as ScpmSim
 import tools.Archivist as Archivist
 import tools.Display as Display
 import tools.Configs.ConfigExp2 as Config
+import pprint
 
 
 def normalized_number(size: int, number: int) -> str:
@@ -18,11 +19,8 @@ def normalized_number(size: int, number: int) -> str:
     return tmp + n
 
 
-def main():
+def run_sims():
     sim = None
-    # model = 'dipm'
-    # model = 'scpm'
-
     models = ['dipm', 'scpm']
 
     for model in models:
@@ -43,9 +41,52 @@ def main():
             sim.init_with_config(conf)
             sim.run_sim(results)
 
-            # # Before launching data saving as images, we need the tools to analyze the outputs and generate the right map
-            # data = Archivist.load(results)
-            # export = 'img_export/' + model + '_exp2' + filename_ending
-            # Display.display_all_and_save(data['gpi_outputs'], model, export, [0, 1, 2], data['salience'], 0.05)
+
+def analyze_results():
+    # result: {Model: {channel: {index: value}}}
+    results = {str: {int: {float: float}}}
+    channels = Config.config_scpm_exp2()['channels']
+    salience = Config.config_scpm_exp2()['salience']
+    dt = Config.config_scpm_exp2()['dt']
+
+    models = ['dipm', 'scpm']
+
+    # model_res: {Model: {sim_number {channel: {salience: value}}}}
+    model_res = {}
+    for model in models:
+        for sim_number in range(0, 121):
+            filename = 'results/exp2/' + 'results_' + model + '_exp2_' + normalized_number(3, sim_number) + '.p'
+            data = Archivist.load(filename)
+            gpi_outputs = data['gpi_outputs']
+            channel_res = {}
+            for channel in range(0, channels):
+                tmp = []
+                salience_res = {}
+                for s in range(0, len(salience[channel])):
+                    for i in range(0, int(1/dt)):
+                        tmp.append(gpi_outputs[channel][i + s * (1/dt)])
+                    avg = sum(tmp) / len(tmp)
+                    val = salience_res.get(salience[channel][s], [])
+                    val.append(avg)
+                    salience_res.update({salience[channel][s]: val})
+                channel_res.update({channel: salience_res})
+            val = model_res.get(model, {})
+            val.update({sim_number: channel_res})
+            model_res.update({model: val})
+
+    # model_res: {Model: {sim_number {channel: {salience: value}}}}
+    # model_res to compute to results
+    pp = pprint.PrettyPrinter(indent=0)
+    pp.pprint(model_res)
+
+
+def main():
+    analyze_results()
+
+    # # Before launching data saving as images, we need the tools to analyze the outputs and generate the right map
+    # data = Archivist.load(results)
+    # export = 'img_export/' + model + '_exp2' + filename_ending
+    # Display.display_all_and_save(data['gpi_outputs'], model, export, [0, 1, 2], data['salience'], 0.05)
+
 
 main()
